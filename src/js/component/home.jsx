d7f7e6ed38f613a4f3ad16da96bbc673f578270a
jossvo <jossvo26@outlook.com>
2022-12-20 01:24:32 +0000

    ToDo versión with same fetch for add/delete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,38 +6,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
   const [todoList, setTodoList] = useState([]);
-  const [needsFetch,setNeedsFetch]=useState(true)
 
   useEffect(() => {
-    if(needsFetch){
       //Fetch data section
       fetch("https://assets.breatheco.de/apis/fake/todos/user/jossvo")
       .then(resp =>resp.json())
       .then(data=> setTodoList(data));
-      setNeedsFetch(false)
-    }
-  },[needsFetch]);
+  },[]);
 
   const [newTask, setNewTask] = useState("");
   const addTodoItem = (event) => {
     if (event.key === "Enter" && newTask != "") {
       var todos =[...todoList];
       todos.push({"label":newTask,"done":false})
-
-      //Update data with fetch
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/jossvo', {
-        method: "PUT",
-        body: JSON.stringify(todos),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then(resp => {
-        if(resp.ok){
-          setNewTask("");
-          setNeedsFetch(true);
-        }
-      })
+      setTodoList(todos)
+      setNewTask("");
     }
   };
 
@@ -45,17 +28,24 @@ const Home = () => {
     if(todoList.length>1){
       let newTodo = [...todoList];
       newTodo.splice(index, 1);
-    //Update data with deleted list
-      fetch('https://assets.breatheco.de/apis/fake/todos/user/jossvo', {
-        method: "PUT",
-        body: JSON.stringify(newTodo),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      .then(resp => {if(resp.ok)setNeedsFetch(true)})
+      setTodoList(newTodo)
     }else alert("Sorry! The list must have AT LEAST 1 item")
   };
+
+  useEffect(()=>{
+    if(todoList.length!=[]){
+       // declare the data fetching function
+      const fetchData = async () => {
+        const data = await fetch('https://assets.breatheco.de/apis/fake/todos/user/jossvo', {
+          method: "PUT",
+          body: JSON.stringify(todoList),
+          headers: {
+            "Content-Type": "application/json"
+          }})
+      }
+      fetchData()
+    }
+  },[todoList])
 
   return (
     <div className="container">
